838e7ef167527a5267a2bfd2c34b13873c0e3d91
simone codeluppi <simone.codeluppi@gmail.com>
2022-10-10 21:48:18 +0200

    cleaned up

--- {{ cookiecutter.repo_name }}/src/{{ cookiecutter.repo_name }}/_logger_formatting.py
+++ {{ cookiecutter.repo_name }}/src/{{ cookiecutter.repo_name }}/_logger_formatting.py
@@ -9,14 +9,13 @@ from _constants import logger_settings
 
 
 def init_logger():
-    logging.root.handlers = []
+    # logging.root.handlers = []
     log_path = Path(logger_settings.logs_path.value)
     logging.captureWarnings(True)
     date_tag = time.strftime("%y%m%d_%H_%M_%S")
     fname = log_path / (date_tag + "_run.log")
 
-    # Configure the fields to include in the JSON output. message is the main log string itself
-    format_str = "%(asctime)s - %(module)s - %(levelname)-4s - %(lineno)d - %(message)s"
+    format_str = "%(asctime)s - level:%(levelname)-4s - module:%(module)s - function:%(funcName)s - line:%(lineno)d - message:%(message)s"
     formatter = logging.Formatter(fmt=format_str)
 
     # Create one file and one stream handler
@@ -30,69 +29,4 @@ def init_logger():
     logger.addHandler(stream_handler)
     logger.setLevel(logger_settings.logs_level.value)
 
-    # Normally we would attach the handler to the root logger, and this would be unnecessary
-    # logger.propagate = True
-    logger.debug("Created logger")
     return logger
-
-
-
-# import logging
-# from logging import config
-
-# log_config = {
-#     "version":1,
-#     "root":{
-#         "handlers" : ["console", "file"],
-#         "level": "DEBUG"
-#     },
-#     "handlers":{
-#         "console":{
-#             "formatter": "std_out",
-#             "class": "logging.StreamHandler",
-#             "level": "DEBUG"
-#         },
-#         "file":{
-#             "formatter":"std_out",
-#             "class":"logging.FileHandler",
-#             "level":"INFO",
-#             "filename":"all_messages.log"
-#         }
-#     },
-#     "formatters":{
-#         "std_out": {
-#             "format": "%(levelname)s : %(module)s : %(funcName)s : %(message)s",
-#         }
-#     },
-# }
-
-# config.dictConfig(log_config)
-
-
-# [loggers]
-# keys=root
-
-# [handlers]
-# keys=console, file
-
-# [formatters]
-# keys=std_out
-
-# [logger_root]
-# handlers = console, file
-# level = DEBUG
-
-# [handler_console]
-# class = logging.StreamHandler
-# level = DEBUG
-# formatter = std_out
-
-
-# [handler_file]
-# class = logging.FileHandler
-# kwargs = {"filename": "all_messages_conf.log"}
-# level = INFO
-# formatter = std_out
-
-# [formatter_std_out]
-# format = %(levelname)s : %(name)s : %(module)s : %(funcName)s : %(message)s
--- {{ cookiecutter.repo_name }}/src/{{ cookiecutter.repo_name }}/pippo.py
+++
@@ -1,8 +0,0 @@
-import logging
-from _logger_formatting import init_logger
-
-
-logger = init_logger()
-
-def cagnura():
-    logger.debug('rattolomeo basizzi')
